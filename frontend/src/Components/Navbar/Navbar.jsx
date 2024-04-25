2f8b4e9c16fac2b9ccb0974a6d0ad1f93d2288f2
Ezequiel Cruz Collazo <Cruzezequiel81@gmail.com>
2024-04-25 00:29:34 -0400

    Installed react to project. and establish my work enviroment.

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
+const Navbar = () => {
+    return(
+        <div className='navbar'>
+           <div className='nav-logo'>
+                <img src={logo} alt='logo'/>
+                <p>SKYWEAR</p>
+            </div>
+            <ul className='nav-menu'>
+                <li>Shop</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>Kids</li>
+            </ul>
+            <div className='nav-login-cart'>
+                
+            </div> 
+        </div>
+    )
+}
+
+export default Navbar
